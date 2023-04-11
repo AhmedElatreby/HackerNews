987dec427eb891e9e631ab4a8b05bc9f13740800
AhmedElatreby <ahmedsport@hotmail.com>
2023-04-11 01:32:24 +0100

    fc

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' show Client;
+import '../models/item_model.dart';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
+class NewApiProvider {
+  Client client = Client();
+
+  fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json' as Uri);
+    final ids = json.decode(response.body);
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json' as Uri);
+    final parsedJson = json.decode(response.body);
+    return ItemModel.fromJson(parsedJson);
+  }
+}
