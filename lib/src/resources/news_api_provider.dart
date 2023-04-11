156daa4362aa703927c57ff5bd8894086c89152d
AhmedElatreby <ahmedsport@hotmail.com>
2023-04-11 02:25:22 +0100

    Mocking HTTP request test

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -9,13 +9,13 @@ class NewApiProvider {
   Client client = Client();
 
   fetchTopIds() async {
-    final response = await client.get('$_root/topstories.json' as Uri);
+    final response = await client.get(Uri.parse('$_root/topstories.json'));
     final ids = json.decode(response.body);
     return ids;
   }
 
   fetchItem(int id) async {
-    final response = await client.get('$_root/item/$id.json' as Uri);
+    final response = await client.get(Uri.parse('$_root/item/$id.json'));
     final parsedJson = json.decode(response.body);
     return ItemModel.fromJson(parsedJson);
   }
