2d6b20a1bcc43c2a3003507c8a7f4afec9018348
AhmedElatreby <ahmedsport@hotmail.com>
2023-04-13 03:02:35 +0100

    error with ListView.builder (snapshot null type)

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:news/src/blocs/stories_bloc.dart';
 import '../blocs/stories_provider.dart';
 
-
 class NewsList extends StatelessWidget {
   Widget build(context) {
-
-
     final bloc = StoriesProvider.of(context);
 
     bloc.fetchTopIds();
@@ -24,12 +21,14 @@ class NewsList extends StatelessWidget {
       stream: bloc.topIds,
       builder: (context, AsyncSnapshot<List<int>> snapshot) {
         if (!snapshot.hasData) {
-          return const Text('Still waiting on Ids');
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         }
         return ListView.builder(
-          itemCount: snapshot.data!.length ?? 0,
-          itemBuilder: (context, int index) {
-            return Text('${snapshot.data![index]}'.toString() ?? '');
+          itemCount: snapshot.data!.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text('${snapshot.data![index]}');
           },
         );
       },
